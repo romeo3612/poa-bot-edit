748e1b50270fefc00f085f3f37bcf6eb28411aac
romeo3612 <144406690+romeo3612@users.noreply.github.com>
2024-09-08 19:41:20 +0900

    Add changes to run.py

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import uvicorn
 import fire
+import time
 from exchange.utility import settings
 from main import app
 
+time.sleep(2)
 
 def start_server(host="0.0.0.0", port=8000 if settings.PORT is None else settings.PORT):
     app.state.port = port
