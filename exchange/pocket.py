39dcaca7233983327c9a7b14fec457620d153a9f
romeo3612 <144406690+romeo3612@users.noreply.github.com>
2024-09-18 17:33:05 +0900

    다시 디버그

--- exchange/pocket.py
+++ exchange/pocket.py
@@ -43,22 +43,18 @@ def delete(collection, id):
     except:
         raise Exception("DB delete error")
 
- 
-    
-def get_full_list(collection, batch_size=200, query_params=None):  
+
+
+def get_full_list(collection, batch_size=200, query_params=None):
     try:
-        print("DEBUG: Reauthentication 시작")
+        print(f"DEBUG: get_full_list 호출 - collection: {collection}, batch_size: {batch_size}, query_params: {query_params}")
         reauth()
-        print("DEBUG: Reauthentication 완료")
-        response = pb.collection(collection).get_full_list(
+        return pb.collection(collection).get_full_list(
             batch=batch_size, query_params=query_params
         )
-        print(f"DEBUG: PocketBase 응답 - {response}")
-        return response
     except Exception as e:
         print(f"DEBUG: PocketBase에서 get_full_list 호출 중 오류 발생 - {str(e)}")
         raise Exception("DB get_full_list error")
-
 
 
 """
